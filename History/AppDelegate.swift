f2a7c471d5614186929b79d38d20a2fc643697bd
Stuart Hearn <mackoon@me.com>
2015-08-18 10:49:26 +0100

    add test message for merging into master

--- History/AppDelegate.swift
+++ History/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     // DUMMY comment (for Git)
+    // DUMMY change for "newBranch"
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
